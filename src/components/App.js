fc0aab31d9919f2c2dc32fb514f4c5b4d046502c
Justin Shaffer <justinbshaffer91@gmail.com>
2023-07-30 15:51:34 -0700

    Initial commit: - Project environment is configured - src/components directory is created to hold component files - App.js and App.css moved to components - Day, MarketSchedule, Produce, and ProduceList components created in src/components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avery's Organics</h1>
+      <p>Welcome to Avery's Organics. We are a mid-sized farm in Northern Oregon that grows organic produce and sells it at farmers markets throughout town.</p>
+    </div>
+  );
+}
+
+export default App;
